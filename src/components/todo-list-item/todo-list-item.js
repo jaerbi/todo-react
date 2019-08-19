88a83b2e6d50d9ae385160e10f6a6119a4c672f1
Yura Avramenko <jaerbi42@gmail.com>
2019-08-19 16:23:52 +0300

    add: State to component and use setState method

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -4,23 +4,67 @@ import './todo-list-item.css'
 
 export default class TodoListItem extends Component {
 	
+	
+	// constructor() {
+	// 	super();
+	// 	this.onLabelClick = () => {
+	// 		console.log(`DOne: ${ this.props.label }`);
+	// 	}
+	//  this.state = {
+	// 	 done: false
+	//  }
+	// }
+	
+	// need to bind this
+	// onLabelClick() {
+	// 	console.log(`DOne: ${ this.props.label }`);
+	// }
+	
+	state = {
+		done: false,
+		important: false
+	};
+	
+	onLabelClick = () => {
+		this.setState(({ done }) => {
+			return {
+				done: !done
+			}
+		})
+	};
+	
+	onMarkImportant = () => {
+		this.setState((state) => {
+			return {
+				important: !state.important
+			}
+		});
+	};
+	
 	render() {
 		
-		const { label, important = false } = this.props;
-		const colorImp = {
-			color: important ? 'crimson' : 'black',
-			fontWeight: important ? 'bold' : 'normal',
-		};
+		const { label } = this.props;
+		const { done, important } = this.state;
+		let classNames = 'todo-list-item';
+		if (done) {
+			classNames += ' done';
+		}
+		
+		if (important) {
+			classNames += ' important'
+		}
 		
 		return (
-			<span className="todo-list-item">
+			<span className={ classNames }>
 			<span
-				className="todo-list-item-label"
-				style={colorImp}
-				onClick={() => console.log(`DOne: ${label}`)}
+				className="todo-list-item-label "
+				onClick={ this.onLabelClick }
 			>{ label }</span>
 			
-			<button type="button" className="btn btn-outline-success btn-sm float-right">
+			<button
+				type="button"
+				className="btn btn-outline-success btn-sm float-right"
+				onClick={ this.onMarkImportant }>
 				<i className="fa fa-exclamation"/>
 			</button>
 			<button type="button" className="btn btn-outline-danger btn-sm float-right">
